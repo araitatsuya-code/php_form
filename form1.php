f5fd2d66a815dae217423b6f22413cf8a115aa61
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-27 00:55:29 +0900

    Update form1.php

---
+++ form1.php
@@ -0,0 +1,15 @@
+<?php
+$subject = "お仕事に関するお問い合わせ";
+$body = "お見積もりの件で、連絡をしました。";
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>お問い合わせ</title>
+</head>
+<body>
+<h1><?php echo $subject ?></h1>
+<p><?php echo $body ?></p>
+</body>
+</html>
